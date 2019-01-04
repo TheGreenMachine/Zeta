86be46920b5cc0f2558f6ab076cba34fa187df94
Andrew Hou <andrewfhou@gmail.com>
2019-01-04 16:20:57 -0600

    updated gitignore, package delcarations

--- src/main/java/frc/team1816/robot/Main.java
+++ src/main/java/frc/team1816/robot/Main.java
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot;
+package frc.team1816.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
 
