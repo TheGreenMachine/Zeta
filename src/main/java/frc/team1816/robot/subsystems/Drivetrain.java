80b6a171a54e39f44515e8e17fa714796254dae5
Andrew Hou <andrewfhou@gmail.com>
2019-01-08 16:14:16 -0600

    added additional follower talons to Drivetrain

--- src/main/java/frc/team1816/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team1816/robot/subsystems/Drivetrain.java
@@ -5,44 +5,43 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/*
-   Base subsystem
- */
-
-
 public class Drivetrain extends Subsystem {
 
-    private TalonSRX rightMain, rightSlave, leftMain, leftSlave;
+    private TalonSRX rightMain, rightSlaveOne, rightSlaveTwo;
+    private TalonSRX leftMain, leftSlaveOne, leftSlaveTwo;
 
-    public Drivetrain(int rightMain, int rightSlave, int leftMain, int leftSlave) {
+    public Drivetrain(int rightMain, int rightSlaveOne, int rightSlaveTwo, int leftMain, int leftSlaveOne, int leftSlaveTwo) {
         super();
-        this.rightMain = new TalonSRX(rightMain);
-        this.rightSlave= new TalonSRX(rightSlave);
-        this.leftMain = new TalonSRX(leftMain);
-        this.leftSlave= new TalonSRX(leftSlave);
 
-        this.rightSlave.set(ControlMode.Follower, rightMain);
-        this.leftSlave.set(ControlMode.Follower, leftMain);
+        this.leftMain = new TalonSRX(leftMain);
+        this.leftSlaveOne = new TalonSRX(leftSlaveOne);
+        this.leftSlaveTwo = new TalonSRX(leftSlaveTwo);
+
+        this.rightMain = new TalonSRX(rightMain);
+        this.rightSlaveOne = new TalonSRX(rightSlaveOne);
+        this.rightSlaveTwo = new TalonSRX(rightSlaveTwo);
+
+        this.leftSlaveOne.set(ControlMode.Follower, leftMain);
+        this.leftSlaveTwo.set(ControlMode.Follower, leftMain);
+        this.rightSlaveOne.set(ControlMode.Follower, rightMain);
+        this.rightSlaveTwo.set(ControlMode.Follower, rightMain);
 
         this.leftMain.setInverted(true);
-        this.leftSlave.setInverted(true);
+        this.leftSlaveOne.setInverted(true);
+        this.leftSlaveTwo.setInverted(true);
 
-        this.rightMain.setNeutralMode(NeutralMode.Brake);
-        this.rightSlave.setNeutralMode(NeutralMode.Brake);
         this.leftMain.setNeutralMode(NeutralMode.Brake);
-        this.leftSlave.setNeutralMode(NeutralMode.Brake);
-
+        this.leftSlaveOne.setNeutralMode(NeutralMode.Brake);
+        this.leftSlaveTwo.setNeutralMode(NeutralMode.Brake);
+        this.rightMain.setNeutralMode(NeutralMode.Brake);
+        this.rightSlaveOne.setNeutralMode(NeutralMode.Brake);
+        this.rightSlaveTwo.setNeutralMode(NeutralMode.Brake);
     }
 
     public void setDrivetrain(double leftPower, double rightPower) {
-        this.rightMain.set(ControlMode.PercentOutput, rightPower);
         this.leftMain.set(ControlMode.PercentOutput, leftPower);
-
-
+        this.rightMain.set(ControlMode.PercentOutput, rightPower);
     }
-
-
-
 
     @Override
     protected void initDefaultCommand() {
