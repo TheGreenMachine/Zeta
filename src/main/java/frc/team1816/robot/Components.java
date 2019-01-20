078e0d1a20a4ad8142327b52ed137370179b200f
Richik SC <sc.richik@gmail.com>
2019-01-20 10:45:25 -0600

    Remove double blank lines

--- src/main/java/frc/team1816/robot/Components.java
+++ src/main/java/frc/team1816/robot/Components.java
@@ -39,5 +39,4 @@ public class Components {
         return instance;
     }
 
-
 }
--- src/main/java/frc/team1816/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/team1816/robot/subsystems/ExampleSubsystem.java
@@ -10,7 +10,6 @@ public class ExampleSubsystem extends Subsystem {
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-
     public void initDefaultCommand() {
         // TODO: Set the default command, if any, for a subsystem here. Example:
         //    setDefaultCommand(new MySpecialCommand());
