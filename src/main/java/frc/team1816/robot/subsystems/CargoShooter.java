b464b1efa4b67a629f856687f41e60aade1c415a
Richik SC <sc.richik@gmail.com>
2019-02-03 11:42:19 -0600

    Refactor cargo shooter to use RobotFactory and conform to project conventions

--- src/main/java/frc/team1816/robot/subsystems/CargoShooter.java
+++ src/main/java/frc/team1816/robot/subsystems/CargoShooter.java
@@ -1,35 +1,39 @@
 package frc.team1816.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.IMotorController;
+import com.ctre.phoenix.motorcontrol.IMotorControllerEnhanced;
+import com.edinarobotics.utils.hardware.RobotFactory;
 import com.edinarobotics.utils.math.Math1816;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+import frc.team1816.robot.Robot;
 
 /**
  * Subsystem for the cargo shooter.
  */
 public class CargoShooter extends Subsystem {
+    private static final String NAME = "cargoshooter";
 
-    private static final String NAME = "CargoShooter";
-
-    private TalonSRX arm;
-    private TalonSRX intake;
+    private IMotorControllerEnhanced arm;
+    private IMotorController intake;
 
     private double armPosition;
     private double armVelocity;
     private double intakeVelocity;
 
+    // TODO: Measure true min and max
     public static final double ARM_POSITION_MIN = 0;
     public static final double ARM_POSITION_MAX = 2048;
 
-    private boolean outputsChanged = false;
+    private boolean outputsChanged;
 
-    public CargoShooter(int armTalonId, int intakeTalonId) {
+    public CargoShooter() {
         super(NAME);
+        RobotFactory factory = Robot.factory;
 
-        this.arm = new TalonSRX(armTalonId);
-        this.intake = new TalonSRX(intakeTalonId);
+        this.arm = (IMotorControllerEnhanced) factory.getMotor(NAME, "arm");
+        this.intake = factory.getMotor(NAME, "intake");
         this.armPosition = 0;
         this.armVelocity = 0.5;
         this.intakeVelocity = 0;
@@ -37,15 +41,11 @@ public class CargoShooter extends Subsystem {
 
         arm.set(ControlMode.Position, 0.0);
         intake.set(ControlMode.PercentOutput, 0.0);
-        arm.configOpenloopRamp(6.0);
+        arm.configOpenloopRamp(6.0, 0);
     }
 
     public void setArmPosition(double armPosition) {
-        this.armPosition = Math1816.coerceValue(
-                ARM_POSITION_MAX,
-                ARM_POSITION_MIN,
-                armPosition * ARM_POSITION_MAX
-        );
+        this.armPosition = armPosition;
         outputsChanged = true;
     }
 
@@ -78,7 +78,14 @@ public class CargoShooter extends Subsystem {
     @Override
     public void periodic() {
         if (outputsChanged) {
-            arm.set(ControlMode.Position, armPosition);
+            // armPosition is a number from [0.0, 1.0] representing a
+            // proportion of the total rotational range of the arm.
+            double armEncoderTicks = Math1816.coerceValue(
+                    ARM_POSITION_MAX,
+                    ARM_POSITION_MIN,
+                    armPosition * ARM_POSITION_MAX
+            );
+            arm.set(ControlMode.Position, armEncoderTicks);
             intake.set(ControlMode.PercentOutput, intakeVelocity);
             outputsChanged = false;
         }
