96b5dcce0cef6ac297f20a6e58a1b572590b4e8a
Andrew Hou <andrewfhou@gmail.com>
2019-02-05 17:29:39 -0600

    fix and add commands

--- src/main/java/frc/team1816/robot/commands/SetCargoShooterCommand.java
+++ src/main/java/frc/team1816/robot/commands/SetCargoShooterCommand.java
@@ -2,21 +2,21 @@ package frc.team1816.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import frc.team1816.robot.Components;
-import frc.team1816.robot.subsystems.CargoCollector;
+import frc.team1816.robot.subsystems.CargoShooter;
 
 public class SetCargoShooterCommand extends Command {
-    private CargoCollector collector;
+    private CargoShooter shooter;
     private double power;
 
     public SetCargoShooterCommand(double power) {
         super("setcargoshootercommand");
-        collector = Components.getInstance().collector;
+        shooter = Components.getInstance().shooter;
         this.power = power;
     }
 
     @Override
     protected void execute() {
-        collector.setIntake(power);
+        shooter.setIntakePower(power);
     }
 
     @Override
