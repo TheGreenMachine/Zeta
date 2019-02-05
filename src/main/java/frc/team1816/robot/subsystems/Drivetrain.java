eeb8c1125783ce295f502fca2d30cb842c346853
Andrew Hou <andrewfhou@gmail.com>
2019-02-04 19:38:27 -0600

    vision drive command work, untested, incorrect kP gain

--- src/main/java/frc/team1816/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/team1816/robot/subsystems/Drivetrain.java
@@ -72,7 +72,7 @@ public class Drivetrain extends Subsystem implements Checkable {
         this.rightSlaveTwo = factory.getMotor(NAME, "rightSlaveTwo", "rightMain");
 
         invertTalons(true);
-        setBrakeMode();
+        enableBrakeMode();
 
         navX = new AHRS(I2C.Port.kMXP);
         System.out.println("NavX Active: " + getGyroStatus());
@@ -92,7 +92,7 @@ public class Drivetrain extends Subsystem implements Checkable {
         }
     }
 
-    public void setBrakeMode() {
+    public void enableBrakeMode() {
         this.leftMain.setNeutralMode(NeutralMode.Brake);
         this.leftSlaveOne.setNeutralMode(NeutralMode.Brake);
         this.leftSlaveTwo.setNeutralMode(NeutralMode.Brake);
@@ -102,7 +102,7 @@ public class Drivetrain extends Subsystem implements Checkable {
         this.rightSlaveTwo.setNeutralMode(NeutralMode.Brake);
     }
 
-    public void setCoastMode() {
+    public void enableCoastMode() {
         this.leftMain.setNeutralMode(NeutralMode.Coast);
         this.leftSlaveOne.setNeutralMode(NeutralMode.Coast);
         this.leftSlaveTwo.setNeutralMode(NeutralMode.Coast);
