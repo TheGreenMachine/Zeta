301bbb006238030895db2baf686c25ef2ed66f00
Andrew Hou <andrewfhou@gmail.com>
2019-02-05 16:17:35 -0600

    integrated gamepad filtering

--- src/main/java/frc/team1816/robot/Controls.java
+++ src/main/java/frc/team1816/robot/Controls.java
@@ -1,6 +1,14 @@
 package frc.team1816.robot;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.edinarobotics.utils.gamepad.FilteredGamepad;
 import com.edinarobotics.utils.gamepad.Gamepad;
+import com.edinarobotics.utils.gamepad.gamepadfilters.DeadzoneFilter;
+import com.edinarobotics.utils.gamepad.gamepadfilters.GamepadFilter;
+import com.edinarobotics.utils.gamepad.gamepadfilters.GamepadFilterSet;
+import com.edinarobotics.utils.gamepad.gamepadfilters.PowerFilter;
 import frc.team1816.robot.commands.ToggleReverseModeCommand;
 import frc.team1816.robot.commands.ToggleSlowModeCommand;
 
@@ -15,8 +23,13 @@ public class Controls {
     private Gamepad gamepadOperator;
 
     private Controls() {
-        gamepadDriver = new Gamepad(0);
-        gamepadOperator = new Gamepad(1);
+        List<GamepadFilter> gamepadFilter = new ArrayList<>();
+        gamepadFilter.add(new DeadzoneFilter(0.05));
+        gamepadFilter.add(new PowerFilter(2));
+        GamepadFilterSet filterSet = new GamepadFilterSet(gamepadFilter);
+
+        gamepadDriver = new FilteredGamepad(0, filterSet);
+        gamepadOperator = new FilteredGamepad(1, filterSet);
 
         gamepadDriver.leftBumper().whenPressed(new ToggleSlowModeCommand(true));
         gamepadDriver.leftBumper().whenReleased(new ToggleSlowModeCommand(false));
